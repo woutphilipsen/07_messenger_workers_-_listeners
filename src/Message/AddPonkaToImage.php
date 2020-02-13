8355ddf09ed28a16da091e9a4798fa2132641f9f
philipsenwout <philipsenwout@outlook.com>
2020-02-13 12:30:32 +0100

    3. handler work

--- src/Message/AddPonkaToImage.php
+++ src/Message/AddPonkaToImage.php
@@ -2,7 +2,20 @@
 
 namespace App\Message;
 
+use App\Entity\ImagePost;
+
 class AddPonkaToImage
 {
+    private $imagePost;
+
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
 
 }
