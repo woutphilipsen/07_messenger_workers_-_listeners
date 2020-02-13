f1cffab52880582af3ea46f56dc4dcd5f840f892
philipsenwout <philipsenwout@outlook.com>
2020-02-13 19:18:26 +0100

    09. embedded message split up the code to delete an image so we can do part sync & part async

---
+++ src/Message/DeletePhotoFile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Message;
+
+class DeletePhotoFile
+{
+    private $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+
+}
