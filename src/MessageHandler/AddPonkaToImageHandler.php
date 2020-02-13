0ed7b364f731b3159641eef295f43db08318f3ab
philipsenwout <philipsenwout@outlook.com>
2020-02-13 12:06:44 +0100

    2. Set up Message class for adding the cat to image

---
+++ src/MessageHandler/AddPonkaToImageHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\AddPonkaToImage;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class AddPonkaToImageHandler implements MessageHandlerInterface
+{
+    public function __invoke(AddPonkaToImage $addPonkaToImage)
+    {
+        dump($addPonkaToImage);
+    }
+}
