5048faaa4a3434deb990e3e936ee80de804e8277
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-14 09:40:11 +0100

    11. retries, build in some randomly failure in add ponka to image and 3retries & command will be cancelled

--- src/MessageHandler/AddPonkaToImageHandler.php
+++ src/MessageHandler/AddPonkaToImageHandler.php
@@ -43,6 +43,10 @@ class AddPonkaToImageHandler implements MessageHandlerInterface, LoggerAwareInte
             return;
         }
 
+        if (rand(0, 10) < 7) {
+            throw new \Exception('I failed randomly!!!!');
+        }
+
         $updatedContents = $this->ponkaficator->ponkafy(
             $this->photoManager->read($imagePost->getFilename())
         );
