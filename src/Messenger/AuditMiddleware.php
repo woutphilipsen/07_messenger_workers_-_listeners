3eaaf42bcf6be63532a79a958a138a97cc01d888
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-14 12:13:08 +0100

    18. logger channel

--- src/Messenger/AuditMiddleware.php
+++ src/Messenger/AuditMiddleware.php
@@ -2,12 +2,20 @@
 
 namespace App\Messenger;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 
 class AuditMiddleware implements MiddlewareInterface
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $messengerAuditLogger)
+    {
+        $this->logger = $messengerAuditLogger;
+    }
+
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if (null === $envelope->last(UniqueIdStamp::class)) {
@@ -17,7 +25,10 @@ class AuditMiddleware implements MiddlewareInterface
         /** @var UniqueIdStamp $stamp */
         $stamp = $envelope->last(UniqueIdStamp::class);
 
-        dump($stamp->getUniqueId());
+        $context = [
+          'id' => $stamp->getUniqueId(),
+          'class' => get_class($envelope->getMessage())
+        ];
 
         return $stack->next()->handle($envelope, $stack);
     }
